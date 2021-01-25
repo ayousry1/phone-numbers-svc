3e7f59c0c9e999415cf073d49648912dab6a124e
ay1.aast@gmail.com <ay1.aast@gmail.com>
2021-01-25 06:40:29 +0200

    adding crossorigin config
    Signed-off-by: ay1.aast@gmail.com <ay1.aast@gmail.com>

--- src/main/java/com/jumia/phonenumber/resources/customer/boundary/CustomerControllerImpl.java
+++ src/main/java/com/jumia/phonenumber/resources/customer/boundary/CustomerControllerImpl.java
@@ -4,11 +4,13 @@ import com.jumia.phonenumber.repositories.customer.entity.Customer;
 import com.jumia.phonenumber.resources.customer.control.CustomerServiceImpl;
 import com.jumia.phonenumber.resources.customer.entity.CustomerModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class CustomerControllerImpl implements CustomerController{
 
     private CustomerServiceImpl customerServiceImpl;
